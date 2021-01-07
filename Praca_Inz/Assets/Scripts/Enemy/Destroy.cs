8e7232198308d96d751a6901e6b8e16964098e09
Crisoo <krykoz05@gmail.com>
2021-01-07 11:57:56 +0100

    pomyłka

---
+++ Praca_Inz/Assets/Scripts/Enemy/Destroy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    
+    public Transform weakPoint;
+    public float weakPointRad = 1.5f;
+    public LayerMask isPlayer;
+
+
+    private void FixedUpdate()
+    {
+        Collider2D colide = Physics2D.OverlapCircle(weakPoint.position, weakPointRad, isPlayer);
+
+
+
+        if (colide)
+        {
+            Score.currScore += 1;
+            Destroy(this.gameObject);
+            this.gameObject.GetComponent<EnemyAI>().alive = false;
+        }
+
+    }
+}
+
---
+++ Praca_Inz/Assets/Scripts/UI/Score.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    public static int currScore;
+   
+    public Text score;
+
+    private void Start()
+    {
+        
+        currScore = 0;
+    }
+
+    public void Update()
+    {
+        score.text = "Score: " + currScore.ToString();
+        Win();
+    }
+
+    void Win ()
+    {
+        if (currScore == 5)
+            SceneManager.LoadScene("WiningScene");
+    }
+}
